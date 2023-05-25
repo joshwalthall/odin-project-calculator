40823596f26a84ba15f7ec442e1ef843eea324b1
Josh Walthall <josh.walthall@gmail.com>
2023-05-25 18:47:35 -0400

    Create operate() function that takes two operands and an operator, then returns appropriate result using the add(), subtract(), multiply(), or divide() function.

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
+let currentOperandOne = 0;
+let currentOperator = '';
+let currentOperandTwo = 0;
+
+function operate (operandOne, operator, operandTwo) {
+    let result = 0;
+    switch (operator) {
+        case '+':
+            result = add(operandOne, operandTwo);
+            break;
+        case '-':
+            result = subtract(operandOne, operandTwo);
+            break;
+        case '*':
+            result = multiply(operandOne, operandTwo);
+            break;
+        case '/':
+            result = divide(operandOne, operandTwo);
+            break;
+    };
+    return result;
+};
+
 function add (operandOne, operandTwo) {
     return operandOne + operandTwo;
-}
+};
 
 function subtract (operandOne, operandTwo) {
     return operandOne - operandTwo;
-}
+};
 
 function multiply (operandOne, operandTwo) {
     return operandOne * operandTwo;
-}
+};
 
 function divide (operandOne, operandTwo) {
     return operandOne / operandTwo;
-}
+};
