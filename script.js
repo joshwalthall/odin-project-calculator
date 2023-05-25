c7ac84ebf011d0b7e0c43bec7c15b02043f15e67
Josh Walthall <josh.walthall@gmail.com>
2023-05-25 18:32:37 -0400

    Create initial add(), subtract(), multiply(), and divide() functions.

---
+++ script.js
@@ -0,0 +1,15 @@
+function add (operandOne, operandTwo) {
+    return operandOne + operandTwo;
+}
+
+function subtract (operandOne, operandTwo) {
+    return operandOne - operandTwo;
+}
+
+function multiply (operandOne, operandTwo) {
+    return operandOne * operandTwo;
+}
+
+function divide (operandOne, operandTwo) {
+    return operandOne / operandTwo;
+}
