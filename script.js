66c4a5322013e5badbdc3c70c724189e4ad10c3d
Josh Walthall <josh.walthall@gmail.com>
2023-05-29 15:31:22 -0400

    Create event listener and updateDisplay() function to change display text to text content of clicked number button

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+const display = document.querySelector('#display');
+const buttonContainer = document.getElementById('button-container');
+const button = document.querySelector('button')
+
+buttonContainer.addEventListener('click', pressButton);
+
 let currentOperandOne = 0;
 let currentOperator = '';
 let currentOperandTwo = 0;
+let displayValue = 0;
 
 function operate (operandOne, operator, operandTwo) {
     let result = 0;
@@ -36,3 +43,14 @@ function multiply (operandOne, operandTwo) {
 function divide (operandOne, operandTwo) {
     return operandOne / operandTwo;
 };
+
+function pressButton (event) {
+    if (event.target.classList.contains('number-button')) {
+        let displayText = event.target.textContent;
+        updateDisplay(displayText);
+    };
+};
+
+function updateDisplay (displayText) {
+    display.textContent = displayText;
+}
