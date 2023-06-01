c92ae712aa9f76153c0624b2f47352b785f91528
Josh Walthall <josh.walthall@gmail.com>
2023-06-01 09:55:54 -0400

    Refactor number button input into handleNumberInput() function

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
-const display = document.querySelector('#display');
-const buttonContainer = document.getElementById('button-container');
-const button = document.querySelector('button')
-
-buttonContainer.addEventListener('click', pressButton);
-
 const memory = {
     displayValue: '',
     operandOne: null,
     operandTwo: null,
     operator: null,
-}
+    currentOperand: null,
+};
 
-function operate (operandOne, operator, operandTwo) {
+const display = document.querySelector('#display');
+const buttonContainer = document.getElementById('button-container');
+const operandMaxLength = 16;
+const operators = ['+', '-', '*', '/'];
+
+buttonContainer.addEventListener('click', pressButton);
+
+function add(operandOne, operandTwo) {
+    return operandOne + operandTwo;
+};
+
+function subtract(operandOne, operandTwo) {
+    return operandOne - operandTwo;
+};
+
+function multiply(operandOne, operandTwo) {
+    return operandOne * operandTwo;
+};
+
+function divide(operandOne, operandTwo) {
+    return operandOne / operandTwo;
+};
+
+function operate(operandOne, operator, operandTwo) {
     let result = 0;
     switch (operator) {
         case '+':
@@ -30,41 +48,38 @@ function operate (operandOne, operator, operandTwo) {
     return result;
 };
 
-function add (operandOne, operandTwo) {
-    return operandOne + operandTwo;
-};
-
-function subtract (operandOne, operandTwo) {
-    return operandOne - operandTwo;
-};
-
-function multiply (operandOne, operandTwo) {
-    return operandOne * operandTwo;
-};
-
-function divide (operandOne, operandTwo) {
-    return operandOne / operandTwo;
-};
-
-function pressButton (event) {
-    if (event.target.classList.contains('number-button')) {
-        let buttonText = event.target.textContent;
-        updateOperand(buttonText);
-        updateDisplay(memory.operandOne);
+function getButtonType(eventTarget) {
+    if (eventTarget.classList.contains('number-button')) {
+        return 'number';
+    } else if (eventTarget.classList.contains('operator-button')) {
+        return 'operator';
+    } else if (eventTarget.id === 'button-clear') {
+        return 'clear';
+    } else if (eventTarget.id === 'button-equals') {
+        return 'equals';
     };
 };
 
-function updateOperand (inputText) {
-    if (memory.operandOne === null) {
-        memory.operandOne = inputText;
-    } else if (memory.operandOne.length == 16) {
-        return;
-    } else {
-        memory.operandOne += inputText;
+function handleNumberInput(numberInput) {
+    if (memory.currentOperand === null) {
+        memory.currentOperand = numberInput;
+    } else if (memory.currentOperand != null && !(memory.currentOperand.length >= operandMaxLength)) {
+        memory.currentOperand += numberInput;
     };
 };
 
-function updateDisplay (displayText) {
+function pressButton(event) {
+    console.log(event.target.id);
+    buttonType = getButtonType(event.target);
+    switch (buttonType) {
+        case 'number':
+            numberInput = event.target.textContent;
+            handleNumberInput(numberInput);
+            updateDisplay(memory.currentOperand);
+    };
+};
+
+function updateDisplay(displayText) {
     memory.displayValue = displayText;
     display.textContent = memory.displayValue;
 };
